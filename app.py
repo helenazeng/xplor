7dc843f1b768b4386695ae1001957fcfd09b913a
Helena Zeng <yqzeng@umich.edu>
2015-09-10 00:32:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from __future__ import absolute_import
-
-import json
-import os
-
-from flask import Flask, render_template, request, redirect, session
-import requests
-
-app = Flask(__name__, static_folder='static', static_url_path='')
-app.requests_session = requests.Session()
-app.secret_key = os.urandom(24)
-
 # use decorators to link the function to a url
 @app.route('/')
 def home():
+	parameters = {
+		'origin': 'Mosher-Jordan Hall, Ann Arbor, MI 48109',
+		'destination': 'New York, NY',
+		'key': 'AIzaSyAj5cCIIpeDtSqKX8gN6UNzTAO-SOmMQVk',
+	}
+	url = 'https://maps.googleapis.com/maps/api/directions/json?parameters'
+
+
+	response = requests.get(url, params=parameters)
+	data = response.json()
+	return render_template('home.html', token=data)
+
+	
+
+@app.route('/price', methods=['GET'])
+def price(): 
+	# uber
 	url = 'https://api.uber.com/v1/products'
 
 	parameters = {
@@ -23,9 +28,17 @@ def home():
 
 	response = requests.get(url, params=parameters)
 
-	data = response.json()
-	return render_template('home.html', token=data)
+	# data = response.json()
 
+	return render_template('home.html', token=response.text)
+	url = config.get('https://api.uber.com/v1/products') + 'estimates/price'
+	parameters = {
+		'server_token': 'gDVK4AzgzSuBcvA5t8oG6P7BfdtdBveGVWGWt1Kq',
+    	'start_latitude': config.get('start_latitude'),
+        'start_longitude': config.get('start_longitude'),
+        'end_latitude': config.get('end_latitude'),
+        'end_longitude': config.get('end_longitude'),
+	}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
