95fc3ea9553c65a9f88469950264bc39f404e8dd
Helena Zeng <yqzeng@umich.edu>
2015-11-05 19:21:15 -0500

    database structure

--- xplor_db.sql
+++ xplor_db.sql
@@ -1,6 +1,7 @@
 
+--
 -- Host: localhost
--- Generation Time: Nov 04, 2015 at 02:13 AM
+-- Generation Time: Nov 06, 2015 at 12:19 AM
 -- Server version: 5.6.27
 -- PHP Version: 5.5.27
 
@@ -44,27 +45,25 @@ CREATE TABLE `flight_info` (
 CREATE TABLE `hotel_info` (
   `HotelID` varchar(45) DEFAULT NULL,
   `Name` varchar(80) DEFAULT NULL,
-  `Location` char(100) DEFAULT NULL,
+  `location` varchar(50) DEFAULT NULL,
   `TotalRate` varchar(45) DEFAULT NULL,
   `CheckInDate` varchar(80) DEFAULT NULL,
   `DetailsUrl` char(200) DEFAULT NULL,
-  `StarRating` varchar(70) DEFAULT NULL
+  `StarRating` varchar(70) DEFAULT NULL,
+  `latitude` varchar(100) DEFAULT NULL,
+  `longitude` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `hotel_info`
 --
 
-INSERT INTO `hotel_info` (`HotelID`, `Name`, `Location`, `TotalRate`, `CheckInDate`, `DetailsUrl`, `StarRating`) VALUES
-('12345', 'hey', 'it works', NULL, NULL, NULL, NULL),
-('12078091', 'Residence Inn Ann Arbor Downtown', '[object Object]', '[object Object]', '2015-10-31', 'http://www.expedia.com/go/hotel/info/12078091/2015-10-31/2015-11-03?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0'),
-('1050440', 'Bell Tower Hotel', '[object Object]', '[object Object]', '2015-11-01', 'http://www.expedia.com/go/hotel/info/1050440/2015-11-01/2015-11-04?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '2.5'),
-('12078091', 'Residence Inn Ann Arbor Downtown', '[object Object]', '[object Object]', '2015-10-31', 'http://www.expedia.com/go/hotel/info/12078091/2015-10-31/2015-11-03?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0'),
-('1050440', 'Bell Tower Hotel', '[object Object]', '[object Object]', '2015-11-01', 'http://www.expedia.com/go/hotel/info/1050440/2015-11-01/2015-11-04?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '2.5'),
-('12078091', 'Residence Inn Ann Arbor Downtown', '[object Object]', '[object Object]', '2015-10-31', 'http://www.expedia.com/go/hotel/info/12078091/2015-10-31/2015-11-03?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0'),
-('12078091', 'Residence Inn Ann Arbor Downtown', '[object Object]', '[object Object]', '2015-10-31', 'http://www.expedia.com/go/hotel/info/12078091/2015-10-31/2015-11-03?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0'),
-('12078091', 'Residence Inn Ann Arbor Downtown', '[object Object]', '[object Object]', '2015-10-31', 'http://www.expedia.com/go/hotel/info/12078091/2015-10-31/2015-11-03?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0'),
-('12078091', 'Residence Inn Ann Arbor Downtown', '[object Object]', '[object Object]', '2015-10-31', 'http://www.expedia.com/go/hotel/info/12078091/2015-10-31/2015-11-03?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0');
+INSERT INTO `hotel_info` (`HotelID`, `Name`, `location`, `TotalRate`, `CheckInDate`, `DetailsUrl`, `StarRating`, `latitude`, `longitude`) VALUES
+('8377936', 'Stone Chalet Bed and Breakfast Inn and Event Center', 'Ann Arbor', '168.54', '2015-11-08', 'http://www.expedia.com/go/hotel/info/8377936/2015-11-08/2015-11-11?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.5', '42.268281', '-83.722783'),
+('458015', 'Wyndham Garden Ann Arbor', 'Ann Arbor', '59.93', '2015-11-10', 'http://www.expedia.com/go/hotel/info/458015/2015-11-10/2015-11-13?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0', '42.28287', '-83.78817'),
+('12078091', 'Residence Inn Ann Arbor Downtown', 'Ann Arbor', '198.69', '2015-11-08', 'http://www.expedia.com/go/hotel/info/12078091/2015-11-08/2015-11-11?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '3.0', '42.281451', '-83.749748'),
+('1050440', 'Bell Tower Hotel', 'Ann Arbor', '206.63', '2015-11-22', 'http://www.expedia.com/go/hotel/info/1050440/2015-11-22/2015-11-25?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '2.5', '42.27942', '-83.73963'),
+('899896', 'Lamp Post Inn', 'Ann Arbor', '72.15', '2015-11-08', 'http://www.expedia.com/go/hotel/info/899896/2015-11-08/2015-11-11?NumRooms=1&NumAdult-Room1=2&tpid=1&eapid=0&langid=1033', '2.0', '42.2594', '-83.71221');
 
 -- --------------------------------------------------------
 
@@ -74,10 +73,9 @@ INSERT INTO `hotel_info` (`HotelID`, `Name`, `Location`, `TotalRate`, `CheckInDa
 
 CREATE TABLE `uber` (
   `origin` varchar(50) DEFAULT NULL,
-  `leaving` varchar(50) DEFAULT NULL,
+  `distance` varchar(100) DEFAULT NULL,
   `destination` varchar(50) DEFAULT NULL,
-  `arriving` varchar(50) DEFAULT NULL,
-  `price` int(100) DEFAULT NULL,
+  `price` varchar(100) DEFAULT NULL,
   `id` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
