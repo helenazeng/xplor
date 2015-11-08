c4fc0844263e7df85df504e34a31c3e99ae50f34
Helena Zeng <yqzeng@umich.edu>
2015-11-08 11:22:37 -0500

    pull data from database

---
+++ json.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+var fs = require('fs');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'mysql',
+  database : 'xplor_local'
+});
+
+
+
+connection.query('select * from flight_info;', function(err, results, fields){
+    if(err) throw err;
+    fs.writeFile('table.json', JSON.stringify(results), function (err) {
+    	if (err) throw err;
+    	console.log('Saved!');
+    });
+});
+
+connection.end();
+ 
+
+
+
+
