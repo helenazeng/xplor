da365048071d1a0143f30df2cd4ea78cd60bf51b
Helena Zeng <yqzeng@umich.edu>
2015-11-14 00:23:39 -0500

    dump data to json file

--- json.js
+++ json.js
@@ -10,8 +10,21 @@ var connection = mysql.createConnection({
 });
 
 
-
 connection.query('select * from flight_info;', function(err, results, fields){
+    if(err) throw err;
+    fs.writeFile('table.json', JSON.stringify(results), function (err) {
+    	if (err) throw err;
+    	console.log('Saved!');
+    });
+});
+connection.query('select * from hotel_info;', function(err, results, fields){
+    if(err) throw err;
+    fs.writeFile('table.json', JSON.stringify(results), function (err) {
+    	if (err) throw err;
+    	console.log('Saved!');
+    });
+});
+connection.query('select * from uber;', function(err, results, fields){
     if(err) throw err;
     fs.writeFile('table.json', JSON.stringify(results), function (err) {
     	if (err) throw err;
